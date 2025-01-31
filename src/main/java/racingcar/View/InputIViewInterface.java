513280b924bf765e539f528e613e451511da3635
unknown <seohas0428@naver.com>
2025-01-31 18:09:33 +0900

    refactor: 파일 구조 수정

--- src/main/java/racingcar/View/InputIViewInterface.java
+++
@@ -1,9 +0,0 @@
-package racingcar.View;
-
-import java.util.Scanner;
-
-public interface InputIViewInterface {
-
-    public abstract String getInput(Scanner scanner);
-
-}
--- src/main/java/racingcar/View/OutputViewInterface.java
+++
@@ -1,6 +0,0 @@
-package racingcar.View;
-
-public interface OutputViewInterface {
-
-    public void printMessage();
-}
